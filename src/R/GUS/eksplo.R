642129119185befa3d9cf505947e7a523709182f
Wojciech Smolak <wojciech.smolak@allegro.pl>
2020-05-18 21:45:09 +0200

    eksploracja #6

---
+++ src/R/GUS/eksplo.R
@@ -0,0 +1,27 @@
+library(dplyr)
+library(ggplot2)
+
+getwd()
+setwd("~/Documents/szkoła/sem4/ADP/amu_adp_final_proj/src/R/GUS/")
+
+
+drogi <- read.csv2("../../../data/GUS/drogi-polska.csv", sep = ";")
+
+drogi %>% 
+  mutate(nowyKod = Kod / 1000) %>%
+  filter(Rodzaje.dróg == 'autostrady', Wartosc != '') %>% 
+  select(Nazwa, Wartosc, Rok) %>% 
+  ggplot(aes(x=Rok, y=Wartosc)) +
+  geom_col() +
+  geom_line(data = wypadki_trend, aes(x=Rok, y=suma))
+
+
+wypadki <- read.csv2("../../../data/GUS/wypadki-polska.csv")
+
+wypadki %>% 
+  mutate(nowyKod = Kod / 1000, rodzaj.jst = if_else(nowyKod %% 100 == 0, 'woj', 'pow')) %>%
+  filter(rodzaj.jst == 'woj', Wskaźniki == 'wypadki drogowe na 100 tys. ludności', Wartosc != '') %>% 
+  select(Wartosc, Rok) %>% 
+  group_by(Rok) %>% 
+  transmute(suma = sum(Wartosc)) -> wypadki_trend
+
